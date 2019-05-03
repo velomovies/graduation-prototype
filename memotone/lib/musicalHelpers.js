a622620febfbb3b3bf5c230c661b00c6620c5c41
velomovies <victor@victorzumpolle.nl>
2019-05-03 16:09:32 +0200

    Add new notes

--- memotone/lib/musicalHelpers.js
+++ memotone/lib/musicalHelpers.js
@@ -1,4 +1,7 @@
-const noteStrings = ['C','C#','D','D#','E','F','F#','G','G#','A','A#','B']
+const noteStrings = ['C3','C3-sharp','D3','D3-sharp','E3','F3','F3-sharp','G3','G3-sharp','A3','A3-sharp','B3',
+'C4','C4-sharp','D4','D4-sharp','E4','F4','F4-sharp','G4','G4-sharp','A4','A4-sharp','B4',
+'C5','C5-sharp','D5','D5-sharp','E5','F5','F5-sharp','G5','G5-sharp','A5','A5-sharp','B5',
+'C6','C6-sharp','D6','D6-sharp','E6','F6','F6-sharp','G6','G6-sharp','A6','A6-sharp','B6']
 
 function noteFromPitch(frequency) {
   const noteNum = 12 * (Math.log(frequency / 440) / Math.log(2))
@@ -13,11 +16,10 @@ function centsOffFromPitch(frequency, note) {
   return Math.floor((1200 * Math.log(frequency / frequencyFromNoteNumber(note))) / Math.log(2))
 }
 
-function getNoteObject(frequency, transpose) {
-  transpose = transpose || 0
+function getNoteObject(frequency) {
   const noteNumber = noteFromPitch(frequency)
   const detune = centsOffFromPitch(frequency, noteNumber)
-  let noteStringNumber = (noteNumber % 12) + transpose
+  let noteStringNumber = noteNumber % 48
   let absDetune
 
   if (detune == 0) {
@@ -27,8 +29,6 @@ function getNoteObject(frequency, transpose) {
   } else if (detune > 0) {
     absDetune = '♯'
   }
-
-  noteStringNumber > 11 ? noteStringNumber = noteStringNumber - 12 : noteStringNumber
 
   return {
     noteName: noteStrings[noteStringNumber],
