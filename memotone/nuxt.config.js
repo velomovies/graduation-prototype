98ba437a84edda335be9063d4b0c245e4c04d235
velomovies <victor@victorzumpolle.nl>
2019-05-22 19:25:13 +0200

    add memotone name and favicon

--- memotone/nuxt.config.js
+++ memotone/nuxt.config.js
@@ -5,7 +5,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'Prototype v0.1',
+    title: 'Memotone',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
