c181e01ec7aa2128c36eab71df6f4e31a4914d1d
velomovies <victor@victorzumpolle.nl>
2019-05-28 13:43:27 +0200

    added new svg's and cleaned some code

---
+++ memotone/pages/music/_slug/_musicId/opties/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="options">
+    <app-header
+      isBack
+      :page="'/archief/opgenomen/opname-1'"
+    />
+    <main class="options__main">
+    </main>
+  </div>
+</template>
+
+<script>
+import appHeader from '../../../../../components/app-header'
+
+export default {
+  components: {
+    appHeader,
+  },
+}
+</script>
+
+<style>
+</style>
