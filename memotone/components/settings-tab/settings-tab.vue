34a2d19e4a9cd945fd37245d09c559a4d3bad865
velomovies <victor@victorzumpolle.nl>
2019-06-04 14:02:54 +0200

    added changing settings

--- memotone/components/settings-tab/settings-tab.vue
+++ memotone/components/settings-tab/settings-tab.vue
@@ -3,12 +3,60 @@
       class="settings-tab"
       :class="{ 'settings-tab--open' : editOpen }"
     >
-      <h1 class="settings-tab__text">Muziekopname</h1>
-      <p class="settings-tab__text meta">Instrument in C</p>
-      <p class="meta">Tempo: 90 bpm</p>
+      <h1 v-if="!editOpen" class="settings-tab__text-h1">
+        {{ musicTitle }}
+      </h1>
+      <input
+        v-if="editOpen"
+        :disabled="!editOpen"
+        class="settings-tab__text-h1"
+        :class="{ 'settings-tab__text-h1--open' : editOpen }"
+        type="text"
+        :value="musicTitle"
+        @change="changeTitle"
+      >
+      <div
+        class="settings-tab__text-instrument"
+        :class="{
+          'settings-tab__text-instrument--open' : editOpen,
+          'settings-tab__text-instrument--small' : selectedInstrument == 0,
+        }"
+      >
+        <select
+          class="settings-tab__text-instrument-input"
+          :disabled="!editOpen"
+          :value="selectedInstrument"
+          @change="changeInstrument"
+        >
+          <option value="0">Zang</option>
+          <option value="1">Instrument in C</option>
+          <option value="2">Instrument in D</option>
+          <option value="3">Instrument in Es</option>
+          <option value="4">Instrument in F</option>
+          <option value="5">Instrument in G</option>
+          <option value="6">Instrument in A</option>
+          <option value="7">Instrument in Bes</option>
+        </select>
+      </div>
+
+      <label
+        class="settings-tab__text-tempo meta"
+      >
+        Tempo:
+        <input
+          class="settings-tab__text-tempo-input"
+          :class="{ 'settings-tab__text-tempo-input--open' : editOpen }"
+          :disabled="!editOpen"
+          type="number"
+          value="90"
+          max="160"
+          min="20"
+          >
+            bpm
+          </label>
       <div class="settings-tab__buttons">
         <transition name="fade">
-          <button v-if="editOpen" @click="editToggle" class="settings-tab__button">
+          <button v-if="editOpen" @click="editClose" class="settings-tab__button">
             <close-icon class="settings-tab__button-icon" />
             <p class="a11y-sr-only">Sluit</p>
           </button>
@@ -34,21 +82,52 @@ export default {
     editIcon,
     saveIcon,
   },
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
   data () {
     return {
       editOpen: false,
+      musicTitleChanged: '',
+      musicInstrumentChanged: -1,
     }
   },
   methods: {
+    editClose () {
+      this.musicTitleChanged = this.title
+      this.editOpen = false
+      this.$emit('toggleEdit', this.editOpen)
+    },
     editToggle () {
       this.editOpen = !this.editOpen
       this.$emit('toggleEdit', this.editOpen)
       return this.editOpen
     },
+    changeTitle (e) {
+      this.musicTitleChanged = e.target.value
+    },
+    changeInstrument (e) {
+      this.musicInstrumentChanged = e.target.value
+    },
   },
   computed: {
     buttonText: function () {
       return this.editOpen ? 'Opslaan' : 'Aanpassingen'
+    },
+    musicTitle: function () {
+      return this.musicTitleChanged ? this.musicTitleChanged : this.title
+    },
+    selectedInstrument: function () {
+      if (this.musicInstrumentChanged >= 0) {
+        return this.musicInstrumentChanged
+      } else if (this.$route.query.selected) {
+        return this.$route.query.selected
+      } else {
+        return 0
+      }
     },
   },
 }
@@ -72,6 +151,71 @@ export default {
 
   .settings-tab__text {
     margin-bottom: .5rem;
+  }
+
+  .settings-tab__text-h1 {
+    font-family: var(--font-sans);
+    margin-bottom: .5rem;
+    width: 100%;
+    background: transparent;
+    font-weight: 400;
+    font-size: var(--font-size-small);
+    line-height: 1.1;
+    border-bottom: .1rem solid transparent;
+    padding: 0 0 .25rem 0;
+    transition: all .3s;
+  }
+
+  .settings-tab__text-h1--open {
+    border-bottom: .1rem solid var(--black);
+  }
+
+  .settings-tab__text-instrument {
+    position: relative;
+    width: 9.5rem;
+  }
+
+  .settings-tab__text-instrument--small {
+    width: 3.5rem;
+  }
+
+  .settings-tab__text-instrument--open::after {
+    content: "";
+    position: absolute;
+    right: 0;
+    top: .25rem;
+    z-index: 1;
+    border: solid var(--theme-color);
+    border-width: 0 .1rem .1rem 0;
+    display: inline-block;
+    padding: .2rem;
+    transform: rotate(45deg);
+  }
+
+  .settings-tab__text-instrument-input {
+    appearance: none;
+    background: transparent;
+    font-family: var(--font-sans);
+    font-size: var(--font-size-smaller);
+    font-weight: 300;
+    display: block;
+    color: var(--theme-color);
+    margin-bottom: .5rem;
+    padding-right: 1.5rem;
+  }
+
+  .settings-tab__text-tempo-input {
+    background: transparent;
+    font-family: var(--font-sans);
+    font-size: var(--font-size-smaller);
+    font-weight: 300;
+    text-align: center;
+    border: .05rem solid transparent;
+    border-radius: .25rem;
+  }
+
+  .settings-tab__text-tempo-input--open {
+    border: .05rem solid var(--theme-color);
   }
 
   .settings-tab__buttons {
